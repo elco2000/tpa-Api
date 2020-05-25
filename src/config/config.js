8643d34bb8d6d6af4d3af9a5344060351efb25f2
Elco Mussert <elcomussert@gmail.com>
2020-05-25 15:30:39 +0200

    config.js aangepast

--- src/config/config.js
+++ src/config/config.js
@@ -2,9 +2,9 @@ const loglevel = process.env.LOGLEVEL || "trace";
 
 module.exports = {
     dbconfig: {
-        host: process.env.DB_HOST || "",
-        user: process.env.DB_USER || "",
-        database: process.env.DB_DATABASE || "",
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "root",
+        database: process.env.DB_DATABASE || "tpa",
         password: process.env.DB_PASSWORD || "",
         connectionLimit: 10,
     },
