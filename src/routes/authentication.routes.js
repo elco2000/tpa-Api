6130a19520e9cdef9ef1bb39a60886a2af48f8f1
Elco Mussert <elcomussert@gmail.com>
2020-05-28 15:03:27 +0200

    Heroku decode test

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const AuthController = require("../controllers/authentication.controller");
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post("/login", AuthController.validateLogin, AuthController.login);
 router.post(
