a215605436b0cec940e7271373b66596d7dc356f
Elco Mussert <elcomussert@gmail.com>
2020-05-28 20:08:28 +0200

    GetUserByID created (Not testing yet!)

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -7,42 +7,42 @@ const HTMLDecoderEncoder = require("html-encoder-decoder");
 
 module.exports = {
     login(req, res, next) {
-                pool.query(
-                    'SELECT "ID", "First_Name", "Last_Name", "Email", "Password" FROM "user" WHERE "Email" = $1',
-                    [req.body.email],
-                    (err, result, fields) => {
-                        // connection.release();
-                        if (err) {
-                            logger.error("Error: ", err);
-                            res.status(500).json({
-                                error: err.toString(),
-                                datetime: new Date().toISOString(),
-                            });
-                        } else {
-                            logger.info("Result from database: ");
-                            logger.info(result);
+        pool.query(
+            'SELECT "ID", "First_Name", "Last_Name", "Email", "Password" FROM "user" WHERE "Email" = $1',
+            [req.body.email],
+            (err, result, fields) => {
+                // connection.release();
+                if (err) {
+                    logger.error("Error: ", err);
+                    res.status(500).json({
+                        error: err.toString(),
+                        datetime: new Date().toISOString(),
+                    });
+                } else {
+                    logger.info("Result from database: ");
+                    logger.info(result);
 
-                            if (result.rows.length >= 1 && result.rows[0].Password === req.body.password) {
-                                logger.info("password = " + result.rows[0].Password);
-                                logger.info("passwords DID match, sending valid token");
-                                const payload = {
-                                    id: result.rows[0].ID,
-                                };
-                                const userinfo = {
-                                    token: jwt.sign(payload, "secret", { expiresIn: "2h" }),
-                                    username: result.rows[0].First_Name + " " + result.rows[0].Last_Name,
-                                };
-                                res.status(200).json(userinfo);
-                            } else {
-                                logger.info("User not found or password is invalid");
-                                res.status(401).json({
-                                    error: "User not found or password is invalid",
-                                    datetime: new Date().toISOString(),
-                                });
-                            }
-                        }
+                    if (result.rows.length >= 1 && result.rows[0].Password === req.body.password) {
+                        logger.info("password = " + result.rows[0].Password);
+                        logger.info("passwords DID match, sending valid token");
+                        const payload = {
+                            id: result.rows[0].ID,
+                        };
+                        const userinfo = {
+                            token: jwt.sign(payload, "secret", {expiresIn: "2h"}),
+                            username: result.rows[0].First_Name + " " + result.rows[0].Last_Name,
+                        };
+                        res.status(200).json(userinfo);
+                    } else {
+                        logger.info("User not found or password is invalid");
+                        res.status(401).json({
+                            error: "User not found or password is invalid",
+                            datetime: new Date().toISOString(),
+                        });
                     }
-                );
+                }
+            }
+        );
     },
 
     validateLogin(req, res, next) {
@@ -56,7 +56,7 @@ module.exports = {
         } catch (ex) {
             res
                 .status(400)
-                .json({ error: ex.toString(), datetime: new Date().toISOString() });
+                .json({error: ex.toString(), datetime: new Date().toISOString()});
         }
     },
 
@@ -64,33 +64,32 @@ module.exports = {
         logger.info("register");
         logger.info(req.body);
 
-                let { firstname, lastname, email, password } = req.body;
+        let {firstname, lastname, email, password} = req.body;
 
 
-
-                pool.query(
-                     'INSERT INTO "user" ("First_Name", "Last_Name", "Email", "Password", "RoleID") VALUES($1, $2, $3, $4, $5)',
-                    [firstname, lastname, email.toLowerCase(), password, 2],
-                    (err, rows, fields) => {
-                        if (err) {
-                            logger.error("Error: " + err);
-                            res.status(400).json({
-                                error: "This username has already been taken.",
-                                datetime: new Date().toISOString(),
-                            });
-                        } else {
-                            logger.trace(rows);
-                            const payload = {
-                                id: rows.insertId,
-                            };
-                            const userinfo = {
-                                token: jwt.sign(payload, "secret", { expiresIn: "2h" }),
-                                username: firstname + " " + lastname,
-                            };
-                            res.status(200).json(userinfo);
-                        }
-                    }
-                );
+        pool.query(
+            'INSERT INTO "user" ("First_Name", "Last_Name", "Email", "Password", "RoleID") VALUES($1, $2, $3, $4, $5)',
+            [firstname, lastname, email.toLowerCase(), password, 2],
+            (err, rows, fields) => {
+                if (err) {
+                    logger.error("Error: " + err);
+                    res.status(400).json({
+                        error: "This username has already been taken.",
+                        datetime: new Date().toISOString(),
+                    });
+                } else {
+                    logger.trace(rows);
+                    const payload = {
+                        id: rows.insertId,
+                    };
+                    const userinfo = {
+                        token: jwt.sign(payload, "secret", {expiresIn: "2h"}),
+                        username: firstname + " " + lastname,
+                    };
+                    res.status(200).json(userinfo);
+                }
+            }
+        );
     },
 
     validateRegister(req, res, next) {
@@ -100,27 +99,28 @@ module.exports = {
                 datetime: new Date().toISOString(),
             });
         } else {
-        try {
-            assert(
-                typeof req.body.firstname === "string",
-                "firstname must be a string."
-            );
-            assert(
-                typeof req.body.lastname === "string",
-                "lastname must be a string."
-            );
-            assert(typeof req.body.email === "string", "email must be a string.");
-            assert(
-                typeof req.body.password === "string",
-                "password must be a string."
-            );
+            try {
+                assert(
+                    typeof req.body.firstname === "string",
+                    "firstname must be a string."
+                );
+                assert(
+                    typeof req.body.lastname === "string",
+                    "lastname must be a string."
+                );
+                assert(typeof req.body.email === "string", "email must be a string.");
+                assert(
+                    typeof req.body.password === "string",
+                    "password must be a string."
+                );
 
-            next();
-        } catch (ex) {
-            res
-                .status(400)
-                .json({ error: ex.toString(), datetime: new Date().toISOString() });
-        } }
+                next();
+            } catch (ex) {
+                res
+                    .status(400)
+                    .json({error: ex.toString(), datetime: new Date().toISOString()});
+            }
+        }
     },
 
     validateToken(req, res, next) {
@@ -151,4 +151,33 @@ module.exports = {
             }
         });
     },
+
+    getUserById(req, res, next) {
+        logger.trace("Get aangeroepen op /user/:userId");
+        const userId = parseInt(req.params.userId);
+
+        pool.query(
+            'SELECT "First_Name", "Last_Name", "Email" FROM "user" WHERE "ID" = $1',
+            [userId],
+            (error, results, fields) => {
+                if (error) {
+                    res.status(400).json({
+                        message: "GetById Failed!",
+                        error: error,
+                    });
+                } else if (results.length === 0) {
+                    res.status(404).json({
+                        message:
+                            "User bestaat niet of er zijn geen Users beschikbaar",
+                        error: error,
+                    });
+                } else {
+                    logger.trace("results: ", results);
+                    res.status(200).json({
+                        result: results.rows,
+                    });
+                }
+            }
+        );
+    }
 };
--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
-const AuthController = require("../controllers/authentication.controller");
+const authController = require("../controllers/authentication.controller");
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.post("/login", AuthController.validateLogin, AuthController.login);
+router.post("/login", authController.validateLogin, authController.login);
 router.post(
     "/register",
-    AuthController.validateRegister,
-    AuthController.register
+    authController.validateRegister,
+    authController.register
 );
 
+router.get("/user/:userId", authController.validateToken, authController.getUserById)
 module.exports = router;
